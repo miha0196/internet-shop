5c0bc4650138c389f19c8b38dbeae116e8de0b77
Michael Khakhel <miha0196@mail.ru>
2020-10-20 15:27:23 +0300

    completed app

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './main.css'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {applyMiddleware, createStore} from 'redux';
+import { createBrowserHistory } from 'history';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { ConnectedRouter, routerMiddleware } from 'connected-react-router';
+import {Provider} from 'react-redux';
+
+import createRootReducer from 'reducers'
+import routes from 'routes';
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            {routes}
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
